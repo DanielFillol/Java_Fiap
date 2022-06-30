052d55cf137bf671d8782e12d907dd213fd544e7
Daniel Fillol <55287657+Darklabel91@users.noreply.github.com>
2022-06-30 19:09:55 -0300

    First Variables

---
+++ HelloWorld/variavbles/Variables_teste.java
@@ -0,0 +1,22 @@
+package variavbles;
+
+public class Variables_teste {
+
+	public static void main(String[] args) {
+		
+		int age = 10;
+		double price = 10.0;
+		char gender = 'M';
+		boolean legalAge = false;
+		
+		System.out.println(age);
+		System.out.println(price);
+		System.out.println(gender);
+		System.out.println(legalAge);
+		
+	
+		
+
+	}
+
+}
