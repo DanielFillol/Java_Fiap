1e0a9f53a7982ffe59da661ba299116b009eb522
Daniel Fillol <55287657+Darklabel91@users.noreply.github.com>
2022-06-30 19:42:38 -0300

    If statement

---
+++ HelloWorld/operators/IfOperator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class IfOperator {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		
+		if(x > 10){
+			System.out.println("X is bigger than 10");
+		} else if (x == 10) {
+			System.out.println("X equals 10");
+		} else {
+			System.out.println("X is smaller than 10");
+		}
+
+	}
+
+}
